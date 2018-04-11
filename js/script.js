362bd8b515de5a9abc7a7449ccb7f684d6c1aac6
alexandre.naime <Tivit@1234!>
2018-04-11 10:19:17 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 var app = new Vue({
     el: '#app',
-    methods: {
+    methods: { 
         openFbLoginDialog () {
-           FB.login(function(response) {
-               console.log(response);
-            }, { scope: 'email' })
+            FB.login(this.checkLoginState, { scope: 'email' })
+        },
+        checkLoginState: function (response) {
+            if (response.status === 'connected') {
+                FB.api('/me', { fields: 'name,email' }, function(profile) {
+                    console.log('Bem vindo ao Fiscaluno, ' + profile.name + '.');
+                });
+            } else if (response.status === 'not_authorized') {
+                console.log('ERRO: não autorizado.');
+            } else {
+                console.log('ERRO: usuário não logado no facebook.');
+            }
         }
     }
 })
